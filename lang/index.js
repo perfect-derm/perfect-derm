0e1902a37305121cc7193ac5328d3d6d2711066f
Patryk Padus <patryk.padus@smartmage.pl>
2019-11-29 00:27:10 +0100

    Engine proper handling

---
+++ lang/index.js
@@ -0,0 +1,25 @@
+export default (context, language) => {
+    var allDataFiles = require.context('../content', true, /^(.*\.(json$))[^.]*$/im);
+    let regex = new RegExp('(' + language + '__)(.*)');
+
+    return new Promise(function (resolve) {    
+        let data = {};
+  
+        allDataFiles.keys().forEach(function(key){
+          let noPrefixData = {};
+          let fileData = allDataFiles(key);
+          
+  
+          Object.keys(fileData).forEach(function(key) {
+            let newKey;
+            if(newKey = key.match(regex)){
+              noPrefixData[newKey[2]] = fileData[key];
+            }
+          });
+  
+          data = {...data, ...noPrefixData};
+        });
+  
+        resolve(data);
+      });
+};
--- lang/pl.js
+++ lang/pl.js
@@ -1,28 +1,7 @@
-var path = require('path');
-// var fileName = module.id().slice(module.id().lastIndexOf(path.sep)+1, module.id().length -3);
 var fileName = 'pl';
-var allDataFiles = require.context("../content", true, /^(.*\.(json$))[^.]*$/im);
-let regex = new RegExp('(' + fileName + '__)(.*)');
+// var fileName = module.id().slice(module.id().lastIndexOf(path.sep)+1, module.id().length -3);
+var importLangData = require('./index');
 
 export default (context) => {
-    return new Promise(function (resolve) {    
-      let data = {};
-
-      allDataFiles.keys().forEach(function(key){
-        let noPrefixData = {};
-        let fileData = allDataFiles(key);
-        
-
-        Object.keys(fileData).forEach(function(key) {
-          let newKey;
-          if(newKey = key.match(regex)){
-            noPrefixData[newKey[2]] = fileData[key];
-          }
-        });
-
-        data = {...data, ...noPrefixData};
-      });
-
-      resolve(data);
-    });
+  return importLangData.default(context, fileName) ;
 }
