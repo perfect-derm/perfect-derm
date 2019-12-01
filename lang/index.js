246c0708e6994c785f5ea15d8fa51d4cb593b555
Patryk Padus <patryk.padus@smartmage.pl>
2019-12-01 19:02:14 +0100

    Tariff proper indexing and translation

--- lang/index.js
+++ lang/index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const delimeter = "__"
+let collectionMatrix = [];
 
 export default (context, language) => {
     var allDataFiles = require.context('../content', true, /^(.*\.(json$))[^.]*$/im);
@@ -7,8 +8,7 @@ export default (context, language) => {
 
     return new Promise(function (resolve) {    
         let data = {};
-      
-
+        
         allDataFiles.keys().forEach(function(filePath){
           let properPrefixEntry = {};
           let fileData = allDataFiles(filePath);
@@ -20,10 +20,30 @@ export default (context, language) => {
 
           let fileName = filePathProcessed.pop();
           fileName = fileName.slice(0, -5);
-          
+
           let languagePrefixByPathAndFilename = filePathProcessed.join(delimeter);
-          languagePrefixByPathAndFilename = languagePrefixByPathAndFilename + delimeter + fileName.toString();
           
+          // Unique ID by dir with _ - use number INT from 0, not key or random stuff to easy loop
+          if(filePathProcessed[0][0] === '_'){
+            var entryInCollectionAndLanguage =  collectionMatrix.filter(function(entry) {
+              return entry.lang == language && entry.collection == filePathProcessed[0];
+            });
+            
+            collectionMatrix.push({
+              lang: language,
+              collection: filePathProcessed[0],
+              index: entryInCollectionAndLanguage.length
+            });
+            
+            filePathProcessed.splice(1, 0, entryInCollectionAndLanguage.length.toString());
+
+            languagePrefixByPathAndFilename = filePathProcessed.join(delimeter);
+
+            properPrefixEntry['$uniqueKeyIndex' + delimeter + filePathProcessed[0]] = entryInCollectionAndLanguage.length.toString();
+          } else {
+            languagePrefixByPathAndFilename = languagePrefixByPathAndFilename + delimeter + fileName.toString();
+          }
+
           Object.keys(fileData).forEach(function(key) {
             let newKey;
             if(newKey = key.match(regex)){
@@ -40,7 +60,7 @@ export default (context, language) => {
   
           data = {...data, ...properPrefixEntry};
         });
-  
+
         resolve(data);
       });
 };
--- pages/tariff.vue
+++ pages/tariff.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>TARIFF</div>
+  <div>
+    <ul class="flex flex-col">
+      <li v-for="(n, index) in parseInt($t('$uniqueKeyIndex___tariff') + 1)" :key="{n, index}" class="flex flex-row">
+        <h2 class="px-4">
+          {{ $t(`_tariff__${ index }__header_lvl`) }} || {{ $t(`_tariff__${ index }__title`) }}
+        </h2>
+        <p class="px-4">
+          {{ $t(`_tariff__${ index }__price`) }}
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
