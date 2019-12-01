c69c50ff38702a8f3dbac8a641a541da8b8ad643
Patryk Padus <patryk.padus@smartmage.pl>
2019-12-01 22:22:55 +0100

    Tariff fix style and id

--- pages/tariff.vue
+++ pages/tariff.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <ul class="flex flex-col">
-      <li v-for="(n, index) in parseInt($t('$uniqueKeyIndex___tariff'))" :key="index" class="flex flex-row">
+      <li v-for="(n, index) in parseInt($t('$uniqueKeyIndex___tariff')) + 1" :key="index" class="flex flex-row">
 
-        <h2 v-if="$t('header_lvl') == '1'" class="px-4">
+        <h2 v-if="$t(`_tariff__${ index }__header_lvl`) == '1'" class="px-4 uppercase text-2xl bg-gray-400 w-full text-center">
           {{ $t(`_tariff__${ index }__title`) }}
         </h2>
 
-        <h3 v-if="$t('header_lvl') == '2'" class="px-4">
+        <h3 v-if="$t(`_tariff__${ index }__header_lvl`) == '2'" class="px-4 text-lg bg-gray-300 w-full text-center">
           {{ $t(`_tariff__${ index }__title`) }}
         </h3>
 
-        <div v-if="$t('header_lvl') == '0'" class="flex flex-row ">
-          <h4>
+        <div v-if="$t(`_tariff__${ index }__header_lvl`) == '0'" class="flex flex-row bg-gray-200 w-full">
+          <h4 class="text-base flex-auto text-left">
             {{ $t(`_tariff__${ index }__title`) }}
           </h4>
-          <p class="px-4">
+          <p class="px-4 flex-auto text-right">
             {{ $t(`_tariff__${ index }__price`) }}
           </p>
         </div>
