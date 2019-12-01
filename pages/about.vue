af2d0af3d10a8bdf9affe937544f3fb2bb3d0f17
Patryk Padus <patryk.padus@smartmage.pl>
2019-12-01 23:07:40 +0100

    About page. Fix misspell

--- pages/about.vue
+++ pages/about.vue
@@ -1,27 +1,42 @@
 <template>
   <div>
-    <div>
-      {{ $md.render(this.$t('about__description__description')) }}
-    </div>
+    <div v-html="$md.render(this.$t('about__description__description'))"></div>
 
     <h2>
       {{ $t('about__section_title__title') }}
     </h2>  
 
-    <div>
-            {{ $md.render(this.$t('about__person_1__description')) }}
-      {{ $t('about__person_1__photo') }}
+    <div class="mb-4">
+      <div v-html="$md.render(this.$t('about__person_1__description'))"></div>
+       <img 
+        v-if="$t('about__person_3__photo') !== ''"
+        :src="require('~/static' + $t('about__person_1__photo') + '?resize&size=200')" 
+        alt="" 
+        class="object-cover"
+        width="200px"
+       />
     </div>
 
-    <div>
-            {{ $md.render(this.$t('about__person_2__description')) }}
-      {{ $t('about__person_2__photo') }}
+    <div class="mb-4">
+      <div v-html="$md.render(this.$t('about__person_2__description'))"></div>
+       <img 
+        v-if="$t('about__person_3__photo') !== ''"
+        :src="require('~/static' + $t('about__person_2__photo') + '?resize&size=200')" 
+        alt="" 
+        class="object-cover"
+        width="200px"
+       />
     </div>
 
-    <div>
-      
-      {{ $md.render(this.$t('about__person_3__description')) }}
-      {{ $t('about__person_3__photo') }}
+    <div class="mb-4">
+      <div v-html="$md.render(this.$t('about__person_3__description'))"></div>
+       <img 
+        v-if="$t('about__person_3__photo') !== ''"
+        :src="require('~/static' + $t('about__person_3__photo') + '?resize&size=200')" 
+        alt="" 
+        class="object-cover"
+        width="200px"
+       />
     </div>
   </div>
 </template>
