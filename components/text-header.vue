c6880b3812aee0670a4c816da4d11ac49b3a6436
Patryk Padus <patryk.padus@smartmage.pl>
2019-12-30 12:32:14 +0100

    Breadcrumb

--- components/text-header.vue
+++ components/text-header.vue
@@ -2,7 +2,7 @@
   <div class="relative">
     <div class="container flex flex-row flex-wrap mb-4 mt-2">
       <a :href="('/').concat(this.$i18n.defaultLocale == this.$i18n.locale ? '' : this.$i18n.locale.concat('/'))" class="mr-2">{{ $t('homepage__breadcrumb__title') }}</a>
-      <span class="block mr-2">></span>
+      <span class="block mr-2">|</span>
       <a class="mr-2">{{ breadcrumb }}</a>
     </div>
 
