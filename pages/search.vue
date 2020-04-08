a7dd646a0c5b8b1c37048b7cad4a352dc113ca79
Triloworld <patryk@padus.pl>
2020-04-08 12:10:21 +0200

    Search

--- pages/search.vue
+++ pages/search.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    SEARCH
+    <div class="gcse-searchbox"></div>
   </div>
 </template>
 
 <script>
   export default {
+    head() {
+      return {
+        script: [
+          { src: 'https://cse.google.com/cse.js?cx=012771998850578004477:qq4hycnubjy', defer: true  }
+        ],
+      }
+    },
     props: {
       'mainClass': {
         type: String,
         default: '',
         required: false
       }
+    },
+    mounted: function(){
+      
     }
   };
 </script>
